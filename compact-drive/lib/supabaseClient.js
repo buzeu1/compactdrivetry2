7dae5ee81e6d24c50cd33c91a7cf1bf3efd95199
buzeu1 <buzaucosmin7@gmail.com>
2025-11-29 02:10:04 +0200

    Update supabaseClient.js

--- compact-drive/lib/supabaseClient.js
+++ compact-drive/lib/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ÎNLOCUIEȘTE CU DATELE TALE DE LA SUPABASE
-const supabaseUrl = 'https://rptzbebrrdxrszaxuztb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdHpiZWJycmR4cnN6YXh1enRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNTc2NzQsImV4cCI6MjA3OTgzMzY3NH0.AJVpyMz3KL2E9zD614mCgQxeIsq-FSzE34AOj0t_54Y';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
