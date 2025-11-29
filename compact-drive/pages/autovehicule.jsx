65839c842da836a27ff004997f03e38d6d7b3901
buzeu1 <buzaucosmin7@gmail.com>
2025-11-30 01:56:19 +0200

    Update autovehicule.jsx

--- compact-drive/pages/autovehicule.jsx
+++ compact-drive/pages/autovehicule.jsx
@@ -514,11 +514,11 @@ const Autovehicule = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Phone size={20} className="text-red-600" />
-                <span>+40 770 935 065</span>
+                <span>+40 768 211 211</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail size={20} className="text-red-600" />
-                <span>contact@compact-drive.ro</span>
+                <span>compactdrivee@gmail.com</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Clock size={20} className="text-red-600" />
