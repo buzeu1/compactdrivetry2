8c93be5cab5d7109abf9b48925417e6896a3ce73
buzeu1 <buzaucosmin7@gmail.com>
2025-11-30 13:55:51 +0200

    Update inscriere.jsx

--- compact-drive/pages/inscriere.jsx
+++ compact-drive/pages/inscriere.jsx
@@ -175,7 +175,7 @@ const Inscriere = () => {
             <a href="/" className="hover:text-red-500 transition">Acasă</a>
             <a href="/despre-noi" className="hover:text-red-500 transition">Despre Noi</a>
             <a href="/#categorii" className="hover:text-red-500 transition">Categorii permise</a>
-            <a href="/autovehicule" className="hover:text-red-500 transition">Tarife</a>
+            <a href="/preturi" className="hover:text-red-500 transition">Tarife</a>
             <a href="/promotii" className="hover:text-red-500 transition">Promoții</a>
             <a href="/informatii-utile" className="hover:text-red-500 transition">Info utile</a>
             <a href="/#contact" className="hover:text-red-500 transition">Contact</a>
@@ -195,7 +195,7 @@ const Inscriere = () => {
             <a href="/" className="block hover:text-red-500 transition">Acasă</a>
             <a href="/despre-noi" className="block hover:text-red-500 transition">Despre Noi</a>
             <a href="/#categorii" className="block hover:text-red-500 transition">Categorii permise</a>
-            <a href="/autovehicule" className="block hover:text-red-500 transition">Tarife</a>
+            <a href="/preturi" className="block hover:text-red-500 transition">Tarife</a>
             <a href="/promotii" className="block hover:text-red-500 transition">Promoții</a>
             <a href="/informatii-utile" className="block hover:text-red-500 transition">Info utile</a>
             <a href="/#contact" className="block hover:text-red-500 transition">Contact</a>
@@ -482,7 +482,7 @@ const Inscriere = () => {
             <div className="space-y-3">
               <a href="/" className="block text-gray-400 hover:text-white transition">Acasă</a>
               <a href="/#categorii" className="block text-gray-400 hover:text-white transition">Categorii permise</a>
-              <a href="/autovehicule" className="block text-gray-400 hover:text-white transition">Tarife</a>
+              <a href="/preturi" className="block text-gray-400 hover:text-white transition">Tarife</a>
               <a href="/promotii" className="block text-gray-400 hover:text-white transition">Promoții</a>
               <a href="/informatii-utile" className="block text-gray-400 hover:text-white transition">Info utile</a>
               <a href="/inscriere" className="block text-red-500">Înscriere</a>
