9af7640f75a2f38fd529cccad840b5c41629629c
buzeu1 <buzaucosmin7@gmail.com>
2025-11-30 14:13:34 +0200

    Update index.jsx

--- compact-drive/pages/index.jsx
+++ compact-drive/pages/index.jsx
@@ -157,10 +157,7 @@ const CompactDrive = () => {
       <Head>
         <title>Școala de șoferi Compact Drive - Roman </title>
         <meta name="description" content="Școala de șoferi Compact Drive din Roman - Formăm șoferi responsabili pentru un trafic bazat pe respect și siguranță" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/logo.jpg" />
       </Head>
 
       <div className="min-h-screen bg-gray-50">
